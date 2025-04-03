d6e2ac3a9e782aa858fd3ab6205aa7db4f39e5e5
bbc1335 <374989167@qq.com>
2025-04-03 11:25:44 +0800

    Add train and test txt files for GC-DET and NEU-DET datasets.

--- train.py
+++ train.py
@@ -1,13 +1,14 @@
 from ultralytics import YOLO
 
-# Load a model
-model = YOLO("TLFINet.yaml")  # build a new model from scratch
+if __name__ == '__main__':
+    # Load a model
+    model = YOLO("TLFINet.yaml")  # build a new model from scratch
 
-# Use the model
-model.train(data="NEU-DET.yaml",
-            epochs=300,
-            batch=32,
-            imgsz=224,
-            patience=100,
-            workers=8,
-            close_mosaic=15)
+    # Use the model
+    model.train(data="NEU-DET.yaml",
+                epochs=300,
+                batch=32,
+                imgsz=224,
+                patience=100,
+                workers=8,
+                close_mosaic=15)
